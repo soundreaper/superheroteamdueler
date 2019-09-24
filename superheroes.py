f7a5b4b346182e37320b18f379139a706c777160
soundreaper@gmail.com <soundreaper@gmail.com>
2019-09-24 10:05:23 -0700

    add Hero class and a way to calculate total attack damage

--- superheroes.py
+++ superheroes.py
@@ -16,6 +16,23 @@ class Armor:
     def block(self):
         return random.randint(0, self.max_block)
 
+class Hero:
+    def __init__(self, name, current_health):
+        self.abilities = []
+        self.armors = []
+        self.name = name
+        self.starting_health = 100
+        self.current_health = current_health
+    
+    def add_ability(self, ability):
+        self.abilities.append(ability)
+    
+    def attack(self):
+        total_attack = 0
+        for ability in self.abilities:
+            total_attack += Ability.attack(ability)
+        return total_attack
+
 if __name__ == "__main__":
     ability = Ability("Great Debugging", 50)
     another_ability = Ability("Smarty Pants", 90)
