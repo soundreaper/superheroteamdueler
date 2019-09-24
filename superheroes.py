a2538ee2fa4b5901388e7c0d31709582759c2423
soundreaper@gmail.com <soundreaper@gmail.com>
2019-09-24 10:04:25 -0700

    add Armor class and add block method inside

--- superheroes.py
+++ superheroes.py
@@ -2,13 +2,24 @@ import random
 
 class Ability:
     def __init__(self, name, attack_strength):
-       self.name = name
-       self.attack_strength = attack_strength
+        self.name = name
+        self.attack_strength = attack_strength
     
     def attack(self):
         return random.randint(0, self.attack_strength)
 
+class Armor:
+    def __init__(self, name, max_block):
+        self.name = name
+        self.max_block = max_block
+
+    def block(self):
+        return random.randint(0, self.max_block)
+
 if __name__ == "__main__":
-    ability = Ability("Debugging Ability", 20)
-    print(ability.name)
-    print(ability.attack())
+    ability = Ability("Great Debugging", 50)
+    another_ability = Ability("Smarty Pants", 90)
+    hero = Hero("Grace Hopper", 200)
+    hero.add_ability(ability)
+    hero.add_ability(another_ability)
+    print(hero.attack())
