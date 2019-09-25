05ba0c1333de8a8d3d39f949ecfc2a25d5af74a5
soundreaper@gmail.com <soundreaper@gmail.com>
2019-09-24 17:12:01 -0700

    add kill/death stat and create team fight method

--- superheroes.py
+++ superheroes.py
@@ -1,5 +1,9 @@
 import random
 
+def inputCheck(input, input_type):
+    if input.isalpha():
+        
+
 class Ability:
     def __init__(self, name, attack_strength):
         self.name = name
@@ -17,14 +21,24 @@ class Armor:
         return random.randint(0, self.max_block)
 
 class Hero:
-    def __init__(self, name, current_health = 100):
+    def __init__(self, name, starting_health = 100):
         self.abilities = []
         self.armors = []
         self.name = name
-        self.current_health = current_health
+        self.starting_health = starting_health
+        self.current_health = starting_health
+        self.deaths = 0
+        self.kills = 0
+        self.status = "Alive"
     
     def add_ability(self, ability):
         self.abilities.append(ability)
+    
+    def add_weapon(self, weapon):
+        self.abilities.append(weapon)
+    
+    def add_armor(self, armor):
+        self.armors.append(armor)
     
     def attack(self):
         total_attack = 0
@@ -49,6 +63,12 @@ class Hero:
             return False
         else:
             return True
+    
+    def add_kill(self, num_kills):
+        self.kills += num_kills
+
+    def add_deaths(self, num_deaths):
+        self.deaths += num_deaths
         
     def fight(self, opponent):
         fighting = True
@@ -67,9 +87,17 @@ class Hero:
             opponent.take_damage(hero1_attack)
 
             if self.is_alive() == False:
+                opponent.add_kill(1)
+                self.add_deaths(1)
+                self.status = "Dead"
+                opponent.status = "Alive"
                 print(opponent.name + " won!")
                 fighting = False
             elif opponent.is_alive() == False:
+                self.add_kill(1)
+                opponent.add_deaths(1)
+                opponent.status = "Dead"
+                self.status = "Alive"
                 print(self.name + " won!")
                 fighting = False
             else:
@@ -97,6 +125,69 @@ class Team:
     def add_hero(self, hero):
         self.heroes.append(hero)
 
+    def attack(self, other_team):
+        alive_hero_list_1 = []
+        alive_hero_list_2 = []
+
+        for hero_1 in self.heroes:
+                if hero_1.status == "Alive":
+                    alive_hero_list_1.append(self.heroes.index(hero_1))
+        
+        for hero_2 in other_team.heroes:
+                if hero_2.status == "Alive":
+                    alive_hero_list_2.append(other_team.heroes.index(hero_2))
+
+        while len(alive_hero_list_1) > 0 and len(alive_hero_list_2) > 0:
+            random_hero_1 = self.heroes[random.choice(alive_hero_list_1)]
+            random_hero_2 = other_team.heroes[random.choice(alive_hero_list_2)]
+
+            random_hero_1.fight(random_hero_2)
+
+            for hero_alivecheck_1 in self.heroes:
+                if hero_alivecheck_1.status == "Dead":
+                    alive_hero_list_1.pop(self.heroes.index(hero_alivecheck_1))
+            
+            for hero_alivecheck_2 in other_team.heroes:
+                if hero_alivecheck_2.status == "Dead":
+                    alive_hero_list_2.pop(other_team.heroes.index(hero_alivecheck_2))
+        
+        if len(alive_hero_list_1) > 0:
+            print("Team " + self.name + " won!")
+        elif len(alive_hero_list_2) > 0:
+            print("Team " + other_team.name + " won!")
+        elif len(alive_hero_list_1) == len(alive_hero_list_2):
+            print("Draw!")
+
+    def revive_heroes(self, health=100):
+        for hero in self.heroes:
+            hero.current_health = hero.starting_health
+
+    def stats(self):
+        for hero in self.heroes:
+            print("Hero: " + hero.name + "| Kills: " + hero.kills + "| Deaths: " + hero.deaths)
+    
+class Arena:
+    def __init__(self):
+        self.team_one = None
+        self.team_two = None
+    
+    def create_ability(self):
+        input("Enter Ability Name: ")
+        input("Enter Ability Attack Strength: ")
+
 if __name__ == "__main__":
-    weapon = Weapon("Axe", 40)
-    print(weapon.attack())
+    team_one = Team("One")
+    jodie = Hero("Jodie Foster")
+    aliens = Ability("Alien Friends", 10000)
+    jodie.add_ability(aliens)
+    team_one.add_hero(jodie)
+    team_two = Team("Two")
+    athena = Hero("Athena")
+    socks = Armor("Socks", 10)
+    athena.add_armor(socks)
+    team_two.add_hero(athena)
+    print(team_two.heroes[0].current_health)
+
+    team_one.attack(team_two)
+
+    print(team_two.heroes[0].current_health)
