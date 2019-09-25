42b7b06049a474e8a58cc9e151982c0505b4e8f0
soundreaper@gmail.com <soundreaper@gmail.com>
2019-09-25 13:16:51 -0700

    comment all classes and methods and remove duplicate add armor function

--- superheroes.py
+++ superheroes.py
@@ -1,21 +1,26 @@
 import random
 
+# Class for Ability that defines what an Ability is
 class Ability:
     def __init__(self, name, attack_strength):
         self.name = name
         self.attack_strength = attack_strength
     
+    # Attack method to return an attack value between 0 and the input value
     def attack(self):
         return random.randint(0, self.attack_strength)
 
+# Class for Armor that defines what Armor is
 class Armor:
     def __init__(self, name, max_block):
         self.name = name
         self.max_block = max_block
 
+    # Block method to return a block value between 0 and the input value
     def block(self):
         return random.randint(0, self.max_block)
 
+# Class for Hero that defines what a Hero is
 class Hero:
     def __init__(self, name, starting_health = 100):
         self.abilities = []
@@ -27,45 +32,59 @@ class Hero:
         self.kills = 0
         self.status = "Alive"
     
+    # Method that adds the inputed ability to the Hero's list of abilities
     def add_ability(self, ability):
         self.abilities.append(ability)
     
+    # Method that adds the inputed weapon to the Hero's list of weapons
     def add_weapon(self, weapon):
         self.abilities.append(weapon)
     
+    # Method that adds the inputed armor to the Hero's list of armors
     def add_armor(self, armor):
         self.armors.append(armor)
     
+    # Method that uses the Ability class' Attack method and returns the sum of all abilities "attack"
     def attack(self):
         total_attack = 0
         for ability in self.abilities:
             total_attack += ability.attack()
         return total_attack
     
-    def add_armor(self, armor):
-        self.armors.append(armor)
-    
+    # Method that uses the Armor class' Block method and returns the sum of all armors "block"
     def defend(self):
         total_block = 0
         for armor in self.armors:
             total_block += armor.block()
         return total_block
     
+    '''
+    Method that calculates damage by subtracting the total block from the damage amount and
+    then subtracts that from the Hero's current health
+    '''
     def take_damage(self, damage_amt):
         self.current_health -= (damage_amt - self.defend())
     
+    # Method that checks if the Hero is still alive or not by checking their current health
     def is_alive(self):
         if self.current_health <= 0:
             return False
         else:
             return True
     
+    # Method that counts how many kills a Hero has
     def add_kill(self, num_kills):
         self.kills += num_kills
 
+    # Method that counts how many deaths a Hero has
     def add_deaths(self, num_deaths):
         self.deaths += num_deaths
-        
+    
+    '''
+    Method that handles a fight between two Heros. If neither Hero has abilites, it is considered a "draw".
+    Otherwise, it runs through each attack, defend, and damage calculation function. It then checks which
+    Hero is still alive and adds kills and deaths respectively and also prints the name of the winner.
+    '''
     def fight(self, opponent):
         fighting = True
         while fighting == True:
@@ -99,28 +118,40 @@ class Hero:
             else:
                 continue
 
+# Class that defines what a Weapon is; inherits the same properities as an Ability but uses its own attack function
 class Weapon(Ability):
     def attack(self):
         return random.randint(self.attack_strength//2, self.attack_strength)
 
+# Class that defines what a Team is
 class Team:
     def __init__(self, name):
         self.name = name
         self.heroes = []
     
+    # Method that accepts a Hero name input and removes them from the Team; if the Hero isn't found, returns "0"
     def remove_hero(self, name):
         for hero in self.heroes:
             if hero.name == name:
                 self.heroes.remove(hero)
         return 0
                 
+    # Method that prints the name of each Hero on the Team
     def view_all_heroes(self):
         for hero in self.heroes:
             print(hero.name)
 
+    # Method that accepts a Hero name input and adds them to the Team
     def add_hero(self, hero):
         self.heroes.append(hero)
 
+    '''
+    Method that handles a fight between two Teams. First, it checks to see which Heroes are "alive" on each Team and then
+    appends their index to a new list. Then, a Hero is randomly selected by index from both lists to fight. After the fight
+    is complete, a check occurs to see which Hero died and removes their index from the respective list. Repeats until one
+    Team has no more Heros alive and then returns the name of the winning Team. In the case both Teams have zero Heroes
+    alive at the same time, it returns a "draw".
+    '''
     def attack(self, other_team):
         alive_hero_list_1 = []
         alive_hero_list_2 = []
@@ -154,39 +185,46 @@ class Team:
         elif len(alive_hero_list_1) == len(alive_hero_list_2):
             return "Draw!"
 
+    # Method that revives all Heros on a Team to the inputted starting health or the default "100"
     def revive_heroes(self, health=100):
         for hero in self.heroes:
             hero.current_health = hero.starting_health
             hero.status = "Alive"
 
+    # Method that prints the stats of each Hero on the inputted Team
     def stats(self):
         for hero in self.heroes:
             print("Hero: " + hero.name + " | Kills: " + str(hero.kills) + " | Deaths: " + str(hero.deaths))
-    
+
+# Class that defines what the Arena is (essentially character creation + fighting)    
 class Arena:
     def __init__(self):
         self.team_one = None
         self.team_two = None
         self.winning_team = None
     
+    # Method that accepts user input to create abilities
     def create_ability(self):
         name = input("Enter Ability Name: ")
         strength = int(input("Enter Ability Attack Strength: "))
         ability = Ability(name, strength)
         return ability
     
+    # Method that accepts user input to create weapons
     def create_weapon(self):
         name = input("Enter Weapon Name: ")
         strength = int(input("Enter Weapon Attack Strength: "))
         weapon = Weapon(name, strength)
         return weapon
     
+    # Method that accepts user input to create armors
     def create_armor(self):
         name = input("Enter Armor Name: ")
         block_power = int(input("Enter Blocking Strength: "))
         armor = Armor(name, block_power)
         return armor
     
+    # Method that accepts user input to create Heroes, also calls previous three functions for each Hero
     def create_hero(self):
         name = input("Enter Hero Name: ")
         health = int(input("Enter Hero Health: "))
@@ -221,6 +259,7 @@ class Arena:
         
         return hero
     
+    # Method that accepts user input to create the first Team
     def build_team_one(self):
         name = input("Team 1 Name: ")
         num_of_heroes = int(input("How many heroes?: "))
@@ -232,6 +271,7 @@ class Arena:
         
         self.build_team_one.view_all_heroes()
     
+    # Method that accepts user input to create the second Team
     def build_team_two(self):
         name = input("Team 2 Name: ")
         num_of_heroes = int(input("How many heroes?: "))
@@ -243,9 +283,11 @@ class Arena:
         
         self.build_team_two.view_all_heroes()
     
+    # Method that has the two teams fight each other
     def team_battle(self):
         self.winning_team = self.build_team_one.attack(self.build_team_two)
     
+    # Method that prints who won the fight and the stats of each Hero on both teams; also shows any surviving Heroes
     def show_stats(self):
         print("The winners are: " + self.winning_team)
         
